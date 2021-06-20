1118966f1ebf2d49fd92c3843311a03006a7e405
Cem Karakurt <cem@11sight.com>
2021-06-20 21:38:38 +0300

    update schema file to delete unnecessary company_id

--- db/migrate/20210620113229_create_companies.rb
+++ db/migrate/20210620113229_create_companies.rb
@@ -2,7 +2,6 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
     create_table :companies do |t|
       t.string :name
-      t.integer :company_id, :null => true, :references => [:companies]
       t.timestamps
     end
   end
