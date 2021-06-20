ea24ab8605ebdf5d9e9e3a44b00276e1a165c7eb
Cem Karakurt <cem@11sight.com>
2021-06-20 21:29:55 +0300

    company migration company_id nullable error fixed

--- db/migrate/20210620113229_create_companies.rb
+++ db/migrate/20210620113229_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
     create_table :companies do |t|
       t.string :name
-      t.integer :company_id, :null => false, :references => [:companies]
+      t.integer :company_id, :null => true, :references => [:companies]
       t.timestamps
     end
   end
