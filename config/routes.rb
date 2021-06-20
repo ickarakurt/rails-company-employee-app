3d801829e45d25d56d5f26dc9aacbdfe4fc807f8
Cem Karakurt <cem@11sight.com>
2021-06-20 21:18:27 +0300

    redirect homepage to companies page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => "companies#index"
+
   resources :companies, :only => [:index, :show, :create, :destroy, :new]
   resources :employees, :only => [:index, :show, :create, :destroy, :new]
 end
